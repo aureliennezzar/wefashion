0cfbe9eaa38fba41dfc7e60ac46649d5ef59392d
subskill-anezzar <a.nezzar@subskill.com>
2022-06-21 10:25:54 +0200

    Product Migration filled

--- database/migrations/2022_06_21_080901_create_products_table.php
+++ database/migrations/2022_06_21_080901_create_products_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -14,7 +13,16 @@ return new class extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
+            //Product fields
             $table->id();
+            $table->string('name',100);
+            $table->string('image');
+            $table->text('description');
+            $table->decimal('price', 8, 2);
+            $table->enum('sizes', ['XS', 'S', 'M', 'L', 'XL']);
+            $table->enum('status', ['standard', 'solded']);
+            $table->boolean('published')->default(false);;
+            $table->string('reference',16);
             $table->timestamps();
         });
     }
